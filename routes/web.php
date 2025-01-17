af4e522bd30639886ca576362eade79ef89dc229
Hung Pham hoang <hungph@pgt-solutions.io>
2025-01-17 11:24:37 +0700

    Implement Role

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,32 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('welcome');
-    });
-    Route::resource('projects', ProjectController::class);
-    Route::resource('users', UserController::class);
+    })->name('dashboard');
+//     Route::resource('projects', ProjectController::class);
+    
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group(['middleware' => ['auth', 'role:admin']], function () {
+    Route::resource('users', UserController::class);
+});
+
+Route::group(['middleware' => ['auth', 'role:pm']], function () {
+    Route::resource('projects', ProjectController::class)->only(['index', 'show']);
+});
+
+Route::group(['middleware' => ['auth', 'role:user']], function () {
+    // Route::get('tasks', [TaskController::class, 'index']);
 });
 
 require __DIR__ . '/auth.php';
