176e30bca6d3b63bdd381aa2a9146ddd3fc8d0c7
Hung Pham hoang <hungph@pgt-solutions.io>
2025-01-20 19:06:40 +0700

    change version of footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -172,7 +172,7 @@
         </div>
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.2.0
+                <b>Version</b> 0.0.1
             </div>
             <strong>Copyright &copy; 2025 <a href="">BlueStar</a>.</strong> All rights
             reserved.
