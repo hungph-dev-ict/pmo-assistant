e8bf64fb5b5ec78b487d9f1655af24e625d74677
Hung Pham hoang <hungph@pgt-solutions.io>
2025-01-24 10:11:20 +0700

    Fix infra

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'PMO Assistant'))</title>
-    @vite(['resources/css/fontawesome-free/css/all.min.css'])
+    @vite(['resources/js/plugins/fontawesome-free/css/all.min.css'])
     <link rel="stylesheet" href="{{ asset('build/css/plugins/flag-icon-css.css') }}">
     <!-- Font Awesome Icons -->
     {{-- <script src="https://code.jscharting.com/latest/jscharting.js"></script> --}}
