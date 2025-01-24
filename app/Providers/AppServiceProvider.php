c04140b4d71652692f38d1d4249f65a977621f7d
Hung Pham hoang <hungph@pgt-solutions.io>
2025-01-24 22:05:34 +0700

    Change https to http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (env('APP_ENV') !== 'local') {
+        if (env('APP_ENV') !== 'local') {
             // Force all URLs to use HTTPS in production
             URL::forceScheme('https');
-        // }
+        }
     }
 }
